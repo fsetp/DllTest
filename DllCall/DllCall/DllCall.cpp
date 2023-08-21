60e698a00d86d7a8db91fc8df72e8b7fb8189562
FREEDOM <tools@fsetp.com>
2023-08-21 11:13:13 +0900

    Add files via upload

---
+++ DllCall/DllCall/DllCall.cpp
@@ -0,0 +1,15 @@
+﻿// DllCall.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+
+#include "DllTest.h"
+
+int main()
+{
+	int a = 10;
+	int b = 33;
+	int c = Mutiply(a, b);
+	printf("%d Time %d = %d\n", a, b, c);
+}
+
---
+++ DllCall/DllCall/DllTest.h
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+#ifndef	DLL_TEST_INCLUDED
+#define	DLL_TEST_INCLUDED
+
+extern "C" __declspec(dllexport) int Mutiply(int a, int b);
+
+#endif	// DLL_TEST_INCLUDED
